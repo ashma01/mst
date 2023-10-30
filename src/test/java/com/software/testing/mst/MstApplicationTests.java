1274377cee990dd5287c25e44dc795dab96dd2cf
Ashma Parveen <ashmaparveen@utexas.edu>
2023-10-30 14:16:34 -0500

    initial commit

---
+++ src/test/java/com/software/testing/mst/MstApplicationTests.java
@@ -0,0 +1,13 @@
+package com.software.testing.mst;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MstApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
